cb09fb1febe18442630d2182c044ef74830cd640
Sam Gubernikoff <sgubernikoff@gmail.com>
2021-12-30 12:44:26 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Sam is the champion";
+document.body.append(newHeader);
